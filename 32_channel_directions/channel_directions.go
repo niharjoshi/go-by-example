c672cb94538713b401edfad65e01357bb1a184e6
niharjoshi <nihar_joshi@hotmail.com>
2025-05-01 11:34:39 -0500

    channel directions

---
+++ 32_channel_directions/channel_directions.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+
+func main() {
+
+	pings, pongs := make(chan string, 1), make(chan string, 1)
+
+	ping(pings, "passed message")
+	pong(pings, pongs)
+
+	fmt.Println(<-pongs)
+
+}
